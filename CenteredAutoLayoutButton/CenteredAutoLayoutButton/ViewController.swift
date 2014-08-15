2b249a97f6c485e4ee04677861add38d569ffba4
Michael Mellinger <mmellinger66@gmail.com>
2014-08-14 23:11:59 -0400

    initial commit of CenteredAutoLayoutButton

---
+++ CenteredAutoLayoutButton/CenteredAutoLayoutButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CenteredAutoLayoutButton
+//
+//  Created by Michael Mellinger on 8/14/14.
+//  Copyright (c) 2014 h4labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
