5d7b72f85e8c4d835bf9521af06d3e53b1328931
Michael Mellinger <mmellinger66@gmail.com>
2014-08-14 23:21:19 -0400

    Create button programmatically and add it with Autolayout

--- CenteredAutoLayoutButton/CenteredAutoLayoutButton/ViewController.swift
+++ CenteredAutoLayoutButton/CenteredAutoLayoutButton/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    var centeredButton: UIButton!
+
+    
+    func addButton() {
+        centeredButton = UIButton()
+        centeredButton.setTitle("Am I centered?", forState: .Normal)
+        centeredButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        centeredButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        centeredButton.addTarget(self, action: "pressed:", forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(centeredButton)
+        
+        var viewDictionary:Dictionary = ["centeredButton": centeredButton]
+        
+        let horizontal:[AnyObject]! = NSLayoutConstraint.constraintsWithVisualFormat("H:|-50-[centeredButton]-50-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewDictionary)
+        
+        let vertical:[AnyObject]! = NSLayoutConstraint.constraintsWithVisualFormat("V:|-100-[centeredButton]-100-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewDictionary)
+        self.view.addConstraints(horizontal)
+        self.view.addConstraints(vertical)
+        
+    }
+    
+    func pressed(sender: UIButton!) {
+        var alertView = UIAlertView();
+        alertView.addButtonWithTitle("Ok");
+        alertView.title = "My Title";
+        alertView.message = "Congratulations";
+        alertView.show();
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        addButton()
     }
 
     override func didReceiveMemoryWarning() {
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-swift
+Swift
 =====
+
+These are my small projects to learn Swift.  It's August 14, 2014.  Swift 1.0 ships in about 30 days.
